1ec5c3933cf074d0674a8071908bc3337b226055
Vinícius Paes Marcelino <Seapiniv@gmail.com>
2024-10-28 15:52:51 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let botaoSom = document.querySelector(".botao-som")
+let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
+
+botaoSom.addEventListener("click", ligarSom)
+
+function ligarSom() {
+    video.muted = false
+}
+
+botao.addEventListener("click", mostrarModal)
+modal.addEventListener("click", esconderModal)
+
+function mostrarModal() {
+    modal.style.display = "block"
+}
+
+function esconderModal() {
+    modal.style.display = "none"
+}
